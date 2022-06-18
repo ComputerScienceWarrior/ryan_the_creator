01f0787f83a492da04e579e18a1807603e7839ab
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2022-06-17 18:29:39 -0700

    create Project model and migration

---
+++ db/migrate/20220618011623_create_projects.rb
@@ -0,0 +1,11 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.text :title
+      t.text :caption
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
